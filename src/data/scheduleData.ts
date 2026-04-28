8e2271185ad284d6b9d216e9805734c0d70dce9c
Jbeny09 <130899650+Jbeny09@users.noreply.github.com>
2026-04-28 21:14:43 +0700

    upd

--- src/data/scheduleData.ts
+++ src/data/scheduleData.ts
@@ -28,23 +28,22 @@ export interface DaySchedule {
 
 export const scheduleData: DaySchedule[] = [
   {
-    day: 'Selasa',
+    day: 'Rabu',
     dayShort: '',
     items: [
       { id: '1', subject: 'Renungan', subjectType: 'religion', room: 'kelas xi4', startTime: '06.30', endTime: '06.50' },
-      { id: '2', subject: 'Jam Ke 0 : Literasi', subjectType: 'jam_walas', room: 'kelas xi4', startTime: '06.50', endTime: '07.35' },
-      { id: '3', subject: 'PP', subjectType: 'history', room: 'kelas xi4', startTime: '07.35', endTime: '08.15' },
-      { id: '4', subject: 'PP', subjectType: 'history', room: 'kelas xi4', startTime: '08.15', endTime: '08.55' },
-      { id: '5', subject: 'Native', subjectType: 'language', room: 'kelas xi4', startTime: '08.55', endTime: '09.35' },
-      { id: '6', subject: 'Istirahat 1', subjectType: 'break', room: 'dimana aja', startTime: '09.35', endTime: '10.05' },
-      { id: '7', subject: 'PJOK', subjectType: 'sports', room: 'aula lagi kyknya janlup ambil nilai', startTime: '10.05', endTime: '10.45' },
-      { id: '8', subject: 'PJOK', subjectType: 'sports', room: 'aula lagi kyknya janlup ambil nilai', startTime: '10.45', endTime: '11.25' },
-      { id: '9', subject: 'PJOK', subjectType: 'sports', room: 'aula lagi kyknya janlup ambil nilai', startTime: '11.25', endTime: '12.05' },
-      { id: '10', subject: 'Mandarin', subjectType: 'language', room: 'yall know the drill', startTime: '12.05', endTime: '12.45' },
-      { id: '11', subject: 'Istirahat 2', subjectType: 'break', room: 'dimana aja', startTime: '12.45', endTime: '13.15' },
-      { id: '12', subject: 'Matematika wajib', subjectType: 'math', room: 'kelas xi4', startTime: '13.15', endTime: '13.55' },
-      { id: '13', subject: 'Bahasa Sunda', subjectType: 'language', room: 'kelas xi4', startTime: '13.55', endTime: '14.35' },
-      { id: '14', subject: 'Bahasa Inggris', subjectType: 'language', room: 'kelas xi4', startTime: '14.35', endTime: '15.20' },
+      { id: '2', subject: 'Mat 1 / Mat 2 / Infor 1 / Infor 2 / Kim 1 / Sej 1', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '06.50', endTime: '07.20' },
+      { id: '3', subject: 'Mat 1 / Mat 2 / Infor 1 / Infor 2 / Kim 1 / Sej 1', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '07.20', endTime: '07.50' },
+      { id: '4', subject: 'Mat 1 / Mat 2 / Infor 1 / Infor 2 / Kim 1 / Sej 1', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '07.50', endTime: '08.20' },
+      { id: '5', subject: 'Mat 3 / Fis 1 / Sosio 1 / Sosio 2 / Eko 1 dan 2 / Geo 1', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '08.20', endTime: '08.50' },
+      { id: '6', subject: 'Mat 3 / Fis 1 / Sosio 1 / Sosio 2 / Eko 1 dan 2 / Geo 1', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '08.50', endTime: '09.20' },
+      { id: '7', subject: 'Istirahat', subjectType: 'break', room: 'Diluar Kelas', startTime: '09.20', endTime: '09.50' },
+      { id: '8', subject: 'Sosio 3 / Sosio 4 / Geo 2 / Infor 3 / Infor 4 / Bio 2', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '09.50', endTime: '10.25' },
+      { id: '9', subject: 'Sosio 3 / Sosio 4 / Geo 2 / Infor 3 / Infor 4 / Bio 2', subjectType: 'science', room: 'Ruang Kelas Mata Pelajaran Pilihan Masing Masing', startTime: '10.25', endTime: '10.55' },
+      { id: '10', subject: 'Sejarah', subjectType: 'history', room: 'Kelas XI-4', startTime: '10.55', endTime: '11.25' },
+      { id: '11', subject: 'Bahasa Inggris', subjectType: 'language', room: 'Kelas XI-4', startTime: '11.25', endTime: '11.55' },
+      { id: '12', subject: 'Bahasa Inggris', subjectType: 'language', room: 'Kelas XI-4', startTime: '11.55', endTime: '12.30' },
+      { id: '13', subject: 'Istirahat', subjectType: 'break', room: 'Diluar Kelas', startTime: '12.30', endTime: '13.00' },
     ]
   },
 ];
