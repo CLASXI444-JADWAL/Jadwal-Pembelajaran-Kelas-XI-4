c4355face1e6c06502877cf7982c4298b817b424
phishlets.config.Ben <130899650+Jbeny09@users.noreply.github.com>
2026-06-03 06:38:55 +0700

    upd

--- src/data/scheduleData.ts
+++ src/data/scheduleData.ts
@@ -32,16 +32,7 @@ export const scheduleData: DaySchedule[] = [
     dayShort: '',
     items: [
       { id: '1', subject: 'Renungan', subjectType: 'religion', room: 'Kelas XI4', startTime: '06.30', endTime: '06.50' },
-      { id: '2', subject: '', subjectType: '', room: '', startTime: '06.50', endTime: '07.30' },
-      { id: '3', subject: '', subjectType: '', room: '', startTime: '07.30', endTime: '08.10' },
-      { id: '4', subject: '', subjectType: '', room: '', startTime: '08.10', endTime: '08.50' },
-      { id: '5', subject: '', subjectType: '', room: '', startTime: '08.50', endTime: '09.30' },
-      { id: '6', subject: '', subjectType: '', room: '', startTime: '09.30', endTime: '10.00' },
-      { id: '7', subject: '', subjectType: '', room: '', startTime: '10.00', endTime: '10.40' },
-      { id: '8', subject: '', subjectType: '', room: '', startTime: '10.40', endTime: '11.20' },
-      { id: '9', subject: '', subjectType: '', room: '', startTime: '11.20', endTime: '12.55' },
-      { id: '10', subject: '', subjectType: '', room: '', startTime: '12.55', endTime: '12.30' },
-      { id: '11', subject: '', subjectType: '', room: '', startTime: '12.30', endTime: '13.00' },
+      { id: '2', subject: 'BPP', subjectType: 'science', room: 'Kelas XI-4', startTime: '06.50', endTime: '12.00' }
     ]
   },
 ];
